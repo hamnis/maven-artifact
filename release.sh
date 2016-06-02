34f4d6ea2ad1d3d07b1747cc7e72b8705c01f11a
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-06-02 11:51:46 +0200

    fix release script

--- release.sh
+++ release.sh
@@ -23,11 +23,11 @@ fi
 echo Releasing $1
 if asksure; then
 
-./build.sh
-
 cat > version <<EOF
 $1
 EOF
+
+./build.sh
 
 git add version
 git commit -m "Setting to release $1"
@@ -35,7 +35,7 @@ echo "Tagging $1"
 git tag v$1
 
 cd dist
-gpg -sign -a maven_artifact-$1-py2.py3-none-any.whl
+gpg --sign -a maven_artifact-$1-py2.py3-none-any.whl
 cd ..
 twine upload dist/maven_artifact-$1-py2.py3-none-any.whl
 else
